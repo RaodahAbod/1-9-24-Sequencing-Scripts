7a64c1fd74c14283ebcd641fb7c7743c5dcc4cad
RaodahAbod <raodah.abod@gmail.com>
2024-01-21 15:31:43 -0600

    this works good enough, but I can clean up this script and make it much more efficient. I need to combine each file in a way where it does not repeat the row (column) titles, and combines all the data cleanly without having to use excel as an extra cleanup step

--- CombiningDeduplicationStatistics.R
+++ CombiningDeduplicationStatistics.R
@@ -8,17 +8,44 @@ samples <- c("1_78-1_K27me3_Accutase", "2_78-1_K27me3_Trypsin",
              "3_78-1_K27me3_FF", "4_78-1_K27me3_Scraped",
              "5_H21792_K27ac_Accutase", "6_H21792_K27ac_Trypsin",
              "7_H21792_K9me3_Trypsin", "8_H21792_K9me3_Accutase", 
-             "9_H21792_K9me3_Scraped", "10_78-1_K27ac_Accutase", "11_78-1_K27ac_Scraped")
+             "9_H21792_K9me3_Scraped", "10_78-1_K27ac_Accutase", 
+             "11_78-1_K27ac_Scraped")
 
 order <- c("First Align","After Filter","After DeDupl")
 
 file <- data.frame()  # Create an empty data frame to store results
 for(sample in samples){
+  for(event in order){
+    file <- as.data.frame(t(read_delim(choose.files(), delim = ":", escape_double = FALSE, col_names = FALSE,
+                                       trim_ws = TRUE, skip = 5))) %>% 
+      mutate(Sample = sample, Event = event) %>%
+      select(Sample, Event, V1, V5,V6,V7,V8,V9,V12,V14) %>% 
+      rbind(file,.)
+  }
+}
+write.csv(file, file = "1-9-24 Sequencing Mapping Statistics Pooled.csv")
+
+# i broke it ------------------------------------------------------------------------
+
+
+# load it
+# cbind with other data types
+# goignt o addin the last columns for ewvent and sample name. 
+# at the very end is when i wuill transpose and selct the columns i want
+
+for(sample in samples){
  for(event in order){
-  file <- as.data.frame(t(read_delim(choose.files(), delim = ":", escape_double = FALSE, col_names = FALSE,
-                             trim_ws = TRUE, skip = 5))) %>% 
-   mutate(Sample = sample, Event = event) %>%
-    select(Sample, Event, V1, V5,V6,V7,V8,V9,V12,V14) %>% 
-    rbind(file,.)
+  file <- read_delim(choose.files(), delim = ":", escape_double = FALSE, 
+                                     col_names = FALSE, trim_ws = TRUE, skip = 5) %>%
+    rowwise() %>% mutate(Sample = sample, Event = event) 
+    #inner_join(file,. , by = V1)
+  #   select(Sample, Event, V1, V5,V6,V7,V8,V9,V12,V14) %>% 
+  #   rbind(file,.)
  }
 }
+
+
+tada <- mutate(as.data.frame(file))
+
+
+
